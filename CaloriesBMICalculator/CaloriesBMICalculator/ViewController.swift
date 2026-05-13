72bbfa0ae7b836fb8cca2919cd7241857584fa84
Nawruzbek Ibragimow <nawruzbekibragimow@MacBook-Pro-M5-Nawruzbek.local>
2026-05-13 12:54:01 +0300

    Initial commit

---
+++ CaloriesBMICalculator/CaloriesBMICalculator/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CaloriesBMICalculator
+//
+//  Created by Nawruzbek Ibragimow on 13.05.2026.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
